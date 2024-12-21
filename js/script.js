699948043269005d6a5abbd2d88e7157dce7cb18
yafimahadika7 <yafi.mahadika7@gmail.com>
2024-12-22 01:45:21 +0700

    Perubahan1

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-sidebar').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+});
